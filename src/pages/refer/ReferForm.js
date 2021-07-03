dc2b2d0eaa26b9db8a37c0c9460531d1ec2f8738
Ankit Kumar <ankitkr0696@gmail.com>
2021-07-03 19:44:44 +0530

    input type fix for refer page

--- src/pages/refer/ReferForm.js
+++ src/pages/refer/ReferForm.js
@@ -75,12 +75,12 @@ const ReferForm = () => {
                             </div>
                             <div className='py-3'>
                                 <label htmlFor="referPhone" className="block text-sm font-medium text-gray-700">Phone</label>
-                                <input value={refers.referPhone} onChange={handleChange} required type="text" name="referPhone" id="referPhone" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                <input value={refers.referPhone} onChange={handleChange} required type="tel" name="referPhone" id="referPhone" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
 
                             </div>
                             <div className='py-3'>
                                 <label htmlFor="referEmail" className="block text-sm font-medium text-gray-700">Email Address</label>
-                                <input value={refers.referEmail} onChange={handleChange} required type="text" name="referEmail" id="referEmail" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                <input value={refers.referEmail} onChange={handleChange} required type="email" name="referEmail" id="referEmail" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
 
                             </div>
                             <div className='py-3'>
@@ -99,7 +99,7 @@ const ReferForm = () => {
                             </div>
                             <div className='py-3'>
                                 <label htmlFor="clientPhone" className="block text-sm font-medium text-gray-700">Phone</label>
-                                <input value={refers.clientPhone} onChange={handleChange} required type="text" name="clientPhone" id="clientPhone" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                <input value={refers.clientPhone} onChange={handleChange} required type="tel" name="clientPhone" id="clientPhone" autoComplete="given-name" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
 
                             </div>
                             <div className='py-3'>
