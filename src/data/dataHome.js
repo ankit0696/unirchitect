e5e6283f574178cad59d553e60fb954dade077c2
Ankit Kumar <ankitkr0696@gmail.com>
2021-07-17 21:51:28 +0530

    home data change

--- src/data/dataHome.js
+++ src/data/dataHome.js
@@ -1,196 +1,205 @@
-import { HomeIcon, LightningBoltIcon, OfficeBuildingIcon, PresentationChartLineIcon, SelectorIcon, ShieldCheckIcon } from '@heroicons/react/outline'
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  OfficeBuildingIcon,
+  PresentationChartLineIcon,
+  SelectorIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/outline'
 
 export const launch = {
-    titleLaptop: `Book your free architecture consultation today!`,
-    titlePhone: `Get free consultation today!`
+  titleLaptop: `Book your free architecture consultation today!`,
+  titlePhone: `Get free consultation today!`,
 }
 
 export const hero = {
-    title: 'Architecture Consultancy',
-    titleSecondary: 'online conferencing',
-    description: 'We provide Architectural, Structural, Vastu consultancy, e.t.c. Clear your doubts with our experts over conferencing. Our solution makes your home/building perfect. Schedule an appointment with an expert. Free.'
+  title: 'Architecture Consultancy',
+  titleSecondary: 'online conferencing',
+  description:
+    'We provide complete Architecture,Interior & Construction solution. Clear your Architectural, Structural, Vastu & other doubts with our experts over conferencing. Schedule an appointment with our experts. Free.',
 }
 
 export const details = {
-    description: 'We reveal a world of possibilities to the client through the information presented in number. We save your construction cost by analysing the best material available in market.',
-    points: [
-        {
-            title: 'Get initial budget estimate',
-            description: 'Estimates to adjust design elements within the client’s budget. Our Consulting team uses schematic drawings to make a budget estimate that gets updated with each stage of design.'
-        },
-        {
-            title: 'Get time estimation',
-            description: 'We provide proper time estimate with our expertise considering various factors such as location, weather, manpower, e.t.c. and save time with newer and better technology for construction.'
-        }
-    ]
+  description:
+    'We reveal a world of possibilities to the client through the information presented in number. We save your construction cost by analysing the best material available in market.',
+  points: [
+    {
+      title: 'Get initial budget estimate',
+      description:
+        'Estimates to adjust design elements within the client’s budget. Our Consulting team uses schematic drawings to make a budget estimate that gets updated with each stage of design.',
+    },
+    {
+      title: 'Get time estimation',
+      description:
+        'We provide proper time estimate with our expertise considering various factors such as location, weather, manpower, e.t.c. and save time with newer and better technology for construction.',
+    },
+  ],
 }
 export const members = [
-    {
-        name: 'Ravi R. Pathak',
-        position: 'CEO & Founder',
-        img: 'raunak.jpg',
-    },
-    {
-        name: 'Ankit',
-        position: 'Managing Director',
-        img: 'ankit.jpg',
-
-    },
-    {
-        name: 'Ar. Aditya Raj',
-        position: 'Executive Director',
-        img: 'aditya.jpg',
-    },
-    {
-        name: 'Shubhna Sandhya',
-        position: 'Graphics Design Head',
-        img: 'shubhna.jpg',
-    },
-    {
-        name: 'Shubham',
-        position: 'Chief Marketing Officer',
-        img: 'shubham.jpg',
-    },
-    {
-        name: 'Shivam Kumar',
-        position: 'Tech Lead',
-        img: 'shivam.jpg',
-
-    },
-
+  {
+    name: 'Ravi R. Pathak',
+    position: 'CEO & Founder',
+    img: 'raunak.jpg',
+  },
+  {
+    name: 'Ankit',
+    position: 'Managing Director',
+    img: 'ankit.jpg',
+  },
+  {
+    name: 'Ar. Aditya Raj',
+    position: 'Executive Director',
+    img: 'aditya.jpg',
+  },
+  {
+    name: 'Shubhna Sandhya',
+    position: 'Graphics Design Head',
+    img: 'shubhna.jpg',
+  },
+  {
+    name: 'Shubham',
+    position: 'Chief Marketing Officer',
+    img: 'shubham.jpg',
+  },
+  {
+    name: 'Shivam Kumar',
+    position: 'Tech Lead',
+    img: 'shivam.jpg',
+  },
 ]
 
 export const categories = [
-    {
-        name: 'commercial',
-        image: 'commercial.jpg'
-    },
-    {
-        name: 'educational',
-        image: 'educational.jpg'
-    },
-    {
-        name: 'interior',
-        image: 'interior.jpg'
-    },
-    {
-        name: 'residence',
-        image: 'residence.jpg'
-    },
+  {
+    name: 'commercial',
+    image: 'commercial.jpg',
+  },
+  {
+    name: 'educational',
+    image: 'educational.jpg',
+  },
+  {
+    name: 'interior',
+    image: 'interior.jpg',
+  },
+  {
+    name: 'residence',
+    image: 'residence.jpg',
+  },
 ]
 
 export const features = [
-    {
-        name: 'Architectural & Interior Consultancy',
-        description:
-            'You will get expert advice on architectural design services whether you need urban design, master planning, site planning, or feasibility studies.',
-        icon: HomeIcon,
-    },
-    {
-        name: 'Construction Guidance',
-        description:
-            `Get A-Z construction guidance - don't worry if you're starting from scratch. You'll get strategic modules for construction employers & workers for a smooth workflow.`,
-        icon: OfficeBuildingIcon,
-    },
-    {
-        name: 'Project Consultancy',
-        description:
-            'One-stop solution for your operational, strategic, and technical requirements. Discuss with us your project parameters and develop project plans that takes your business to next level.',
-        icon: PresentationChartLineIcon,
-    },
-    {
-        name: 'MEP Consultancy/HVAC Automtion/Fire Fighting',
-        description:
-            'Optimize your building’s performance and minimize its operating costs with inclusive MEP consulting covering everything from pre-design to post-construction.',
-        icon: ShieldCheckIcon,
-    },
-    {
-        name: 'Strategic Guidance',
-        description:
-            'Get to know the best construction method & process that not only saves time and money but ensures risk control measures are adequate on a construction level and in the worksite to eliminate uncertainties.',
-        icon: LightningBoltIcon,
-    },
-    {
-        name: 'Building Services Consultancy',
-        description:
-            'Building services engineers advise about, design, install and maintain cost-effective and energy efficient systems for building services such as water, lighting, heating, air conditioning, lifts, and telecoms.',
-        icon: SelectorIcon,
-    },
+  {
+    name: 'Architectural & Interior Consultancy',
+    description:
+      'You will get expert advice on architectural design services whether you need urban design, master planning, site planning, or feasibility studies.',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Construction Guidance',
+    description: `Get A-Z construction guidance - don't worry if you're starting from scratch. You'll get strategic modules for construction employers & workers for a smooth workflow.`,
+    icon: OfficeBuildingIcon,
+  },
+  {
+    name: 'Project Consultancy',
+    description:
+      'One-stop solution for your operational, strategic, and technical requirements. Discuss with us your project parameters and develop project plans that takes your business to next level.',
+    icon: PresentationChartLineIcon,
+  },
+  {
+    name: 'MEP Consultancy/HVAC Automtion/Fire Fighting',
+    description:
+      'Optimize your building’s performance and minimize its operating costs with inclusive MEP consulting covering everything from pre-design to post-construction.',
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: 'Strategic Guidance',
+    description:
+      'Get to know the best construction method & process that not only saves time and money but ensures risk control measures are adequate on a construction level and in the worksite to eliminate uncertainties.',
+    icon: LightningBoltIcon,
+  },
+  {
+    name: 'Building Services Consultancy',
+    description:
+      'Building services engineers advise about, design, install and maintain cost-effective and energy efficient systems for building services such as water, lighting, heating, air conditioning, lifts, and telecoms.',
+    icon: SelectorIcon,
+  },
 ]
 
 export const testimonialsText = {
-    title: 'Hear from our clients',
-    description: `We love feedback from our clients.Don't just take our word for it, read from our customer testimonials.`
+  title: 'Hear from our clients',
+  description: `We love feedback from our clients.Don't just take our word for it, read from our customer testimonials.`,
 }
 
 export const testimonials = [
-    {
-        name: 'Mr. Jatin Agarwal',
-        description:
-            'Thank you for your work in making our home a place that our family truly loves, cherish and is continually “wow’d” by Unirchitect. You listened to us, had patience with us, listened to us again and orchestrated a magnificent design that was a creation of our dreams.',
-        location: 'Ranchi, Jharkhand',
-        image: 'jatin.jpg'
-    },
-    {
-        name: 'Mr. Satish Kumar',
-        description:
-            'We are pleased with your service so far. You are young and energetic. This means you may consider to differentiate your service compared with others in your business.',
-        location: 'Texas, USA',
-        image: 'satish.jpg'
-    },
-    {
-        name: 'Ms. Kamna Jha',
-        description:
-            `You guys were meticulous, in constant contact during the project, informed us on specifics as the project was underway, kept close to the project time-line, has reliable and highly qualified workers who explain their job, and enjoy conversing with the homeowner. At no time did we feel dissatisfied or in-the-dark. The entire project from initial discussion of the project to the very end was smooth.
+  {
+    name: 'Mr. Jatin Agarwal',
+    description:
+      'Thank you for your work in making our home a place that our family truly loves, cherish and is continually “wow’d” by Unirchitect. You listened to us, had patience with us, listened to us again and orchestrated a magnificent design that was a creation of our dreams.',
+    location: 'Ranchi, Jharkhand',
+    image: 'jatin.jpg',
+  },
+  {
+    name: 'Mr. Satish Kumar',
+    description:
+      'We are pleased with your service so far. You are young and energetic. This means you may consider to differentiate your service compared with others in your business.',
+    location: 'Texas, USA',
+    image: 'satish.jpg',
+  },
+  {
+    name: 'Ms. Kamna Jha',
+    description: `You guys were meticulous, in constant contact during the project, informed us on specifics as the project was underway, kept close to the project time-line, has reliable and highly qualified workers who explain their job, and enjoy conversing with the homeowner. At no time did we feel dissatisfied or in-the-dark. The entire project from initial discussion of the project to the very end was smooth.
 
 `,
-        location: 'Patna, India',
-        image: 'kamna.jpg'
-    },
-    {
-        name: 'Dr. J. P. Singh',
-        description:
-            'Thank you for your amazing talent, patience, perseverance, creativity and ability to manage challenges and changes all along the way. You did with grace and professionalism that is seldom exhibited in these fast- paced busy lives we all have. We are proud of the work you have accomplished and the way you have made our family feel in our home and what you have given to us for now and into the future. We are eternally thankful!',
-        location: 'Chandigarh, India',
-        image: 'jp.jpg'
-    },
+    location: 'Patna, India',
+    image: 'kamna.jpg',
+  },
+  {
+    name: 'Dr. J. P. Singh',
+    description:
+      'Thank you for your amazing talent, patience, perseverance, creativity and ability to manage challenges and changes all along the way. You did with grace and professionalism that is seldom exhibited in these fast- paced busy lives we all have. We are proud of the work you have accomplished and the way you have made our family feel in our home and what you have given to us for now and into the future. We are eternally thankful!',
+    location: 'Chandigarh, India',
+    image: 'jp.jpg',
+  },
 ]
 
 export const awards = [
-    {
-        name: 'Best of Houzz 2021',
-        description: 'For the number and quality of client reviews on Houzz. Awarded by Houzz CEO & President',
-        image: 'houzz2021.jpg',
-        learnMore: true,
-        learnMoreUrl: 'https://www.houzz.in/pro/raviraunak-pathak/unirchitect'
-    },
-    {
-        name: 'SIH, Government of India',
-        description: 'Winner at SIH. Awarded by Ministry of Road and Transport, Government of India.',
-        image: 'sih.jpg',
-        learnMore: false
-    }
+  {
+    name: 'Best of Houzz 2021',
+    description:
+      'For the number and quality of client reviews on Houzz. Awarded by Houzz CEO & President',
+    image: 'houzz2021.jpg',
+    learnMore: true,
+    learnMoreUrl: 'https://www.houzz.in/pro/raviraunak-pathak/unirchitect',
+  },
+  {
+    name: 'SIH, Government of India',
+    description:
+      'Winner at SIH. Awarded by Ministry of Road and Transport, Government of India.',
+    image: 'sih.jpg',
+    learnMore: false,
+  },
 ]
 
 export const carrerData = {
-    carrerHeroDesc: 'Talent comes in many forms—from designers and planners to strategists and operations specialists—we invite you to continue your career journey with us.',
-    carrerHeroTitle: 'Make your move. Our greatest strength is you.',
+  carrerHeroDesc:
+    'Talent comes in many forms—from designers and planners to strategists and operations specialists—we invite you to continue your career journey with us.',
+  carrerHeroTitle: 'Make your move. Our greatest strength is you.',
 
-    carrerHero: [
-        {
-            title: 'Fill it up',
-            description: 'Our people are our greatest asset. Whether you’re fresh out of school or a seasoned pro, we provide and encourage opportunities for you to grow and do what you love.'
-        },
-        {
-            title: 'Light it',
-            description: 'Blow and go with the flow of design and your passion with us.Start journey from ground to the moon and light up your journey and learn with the flow.'
-        },
-        {
-            title: 'Shoot for the stars',
-            description: 'Whatever your thing is, we’ve got a career path you’ll love.From competitive salaries to healthy snacks and Flex-Fridays, we work hard and play hard too.'
-        }
-
-    ]
+  carrerHero: [
+    {
+      title: 'Fill it up',
+      description:
+        'Our people are our greatest asset. Whether you’re fresh out of school or a seasoned pro, we provide and encourage opportunities for you to grow and do what you love.',
+    },
+    {
+      title: 'Light it',
+      description:
+        'Blow and go with the flow of design and your passion with us.Start journey from ground to the moon and light up your journey and learn with the flow.',
+    },
+    {
+      title: 'Shoot for the stars',
+      description:
+        'Whatever your thing is, we’ve got a career path you’ll love.From competitive salaries to healthy snacks and Flex-Fridays, we work hard and play hard too.',
+    },
+  ],
 }
-
-
